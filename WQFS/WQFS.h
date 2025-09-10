9cc79716c28525455eb4045793263a280462a81b
inhee <devforplayer@gmail.com>
2025-09-10 16:33:29 +0900

    v0.14 WQFS class setup

--- WQFS/WQFS.h
+++ WQFS/WQFS.h
@@ -6,7 +6,21 @@
 
 class WQFS
 {
+private:
+	std::vector<std::string> world_events;
+	std::vector<std::string> npcs;
+	std::vector<std::string> comps;
 
+	void SetCompensation(std::string comp);
+	void CheckConfirmation();
+public:
+	WQFS();
+	~WQFS();
+
+	void AddEvent();
+	void AddNPC();
+	void AddItem();
+	void DoEvent();
 };
 
 #endif
