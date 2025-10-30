b37a5e51cc101576d78de237767c9ef32cacf97a
inhee <devforplayer@gmail.com>
2025-10-30 13:58:51 +0900

    v0.58 Particle Generator 1

---
+++ World_event_n_Quest_Fusion_System/ParticleGenerator.cpp
@@ -0,0 +1,91 @@
+#include "ParticleGenerator.h"
+
+ParticleGenerator::ParticleGenerator(Shader shader, Texture texture, unsigned int _amount) 
+	: pShader(shader), pTexture(texture), amount(_amount) {
+	init();
+}
+
+void ParticleGenerator::init() {
+	unsigned int pbo;
+
+	float particleQuad[] = {
+		0.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 0.0f,
+
+		0.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 1.0f, 1.0f,
+		1.0f, 0.0f, 1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1, &pao);
+	glGenBuffers(1, &pbo);
+	glBindVertexArray(pao);
+	glBindBuffer(GL_VERTEX_ARRAY, pbo);
+	glBufferData(GL_VERTEX_ARRAY, sizeof(particleQuad), particleQuad, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glBindVertexArray(0);
+
+	for (unsigned int i = 0; i < amount; ++i) {
+		particles.push_back(Particle());
+	}
+}
+
+void ParticleGenerator::Update(float dt, GameObject& object, unsigned int newParticle, glm::vec2 offset) {
+	for (unsigned int i = 0; i < newParticle; ++i) {
+		int unusedParticle = FirstUnusedParticle();
+		RespawnParticle(particles[unusedParticle], object, offset);
+	}
+
+	for (unsigned int i = 0; i < amount; ++i) {
+		Particle& p = particles[i];
+
+		if (p.life > 0.0f) {
+			p.life -= dt;
+
+			p.Position -= p.Velocity * dt;
+			p.Color.a -= dt * 2.5f;
+		}
+	}
+}
+
+unsigned int ParticleGenerator::FirstUnusedParticle() {
+	for (unsigned int i = 0; i < amount; ++i) {
+		if (particles[i].life <= 0.0f) {
+			lastUsedParticle = i;
+			return i;
+		}
+	}
+
+	lastUsedParticle = 0;
+	return 0;
+}
+
+void ParticleGenerator::RespawnParticle(Particle& particle, GameObject& object, glm::vec2 offset) {
+	float random = ((rand() % 100) - 50) / 7.0f;
+	float color = 0.7f + ((rand() % 100) / 100.0f);
+
+	particle.Position = object.objPosition + random + offset;
+	particle.Color = glm::vec4(color, color, color, 1.0f);
+	particle.life = 0.15f;
+	particle.Velocity = object.objVelocity * 0.1f;
+}
+
+void ParticleGenerator::Draw() {
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+	pShader.use();
+
+	for (Particle particle : particles) {
+		if (particle.life > 0.0f) {
+			pShader.SetVec2("offest", particle.Position);
+			pShader.SetVec4("color", particle.Color);
+			pTexture.Bind();
+			glBindVertexArray(pao);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+			glBindVertexArray(0);
+		}
+	}
+
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_CONSTANT_ALPHA);
+}
---
+++ World_event_n_Quest_Fusion_System/ParticleGenerator.h
@@ -0,0 +1,42 @@
+#ifndef PARTICLEGENERATOR_H
+#define PARTICLEGENERATOR_H
+
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+#include <iostream>
+#include <vector>
+
+#include "Shader.h"
+#include "Texture.h"
+#include "GameObject.h"
+
+struct Particle 
+{
+	glm::vec2 Position;
+	glm::vec2 Velocity;
+	glm::vec4 Color;
+	float life;
+
+	Particle() : Position(0.0f), Velocity(0.0f), Color(0.0f), life(0.0f) {}
+};
+
+class ParticleGenerator
+{
+private:
+	std::vector<Particle> particles;
+	Shader pShader;
+	Texture pTexture;
+	unsigned int amount;
+	unsigned int pao;
+	unsigned int lastUsedParticle = 0;
+
+	void init();
+	unsigned int FirstUnusedParticle();
+	void RespawnParticle(Particle& particle, GameObject& object, glm::vec2 offset);
+public:
+	ParticleGenerator(Shader shader, Texture texture, unsigned int _amount);
+	void Update(float dt, GameObject &object, unsigned int newParticle, glm::vec2 offset = glm::vec2(0.0f));
+	void Draw();
+};
+
+#endif
